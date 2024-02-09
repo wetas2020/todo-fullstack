c0851c7937c0d3e07a64f81fe72492459d8a61bd
EL GABOURI Saad <elgabouri.saad@gmail.com>
2024-02-09 19:25:01 +0100

    show new form with /todo/new

--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,15 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const knex = require('../db/knex');
 
 /* This router is mounted at https://localhost:3000/todo */
-router.get('/', function (req, res, next) {
+router.get('/', (req, res) => {
     knex.select()
         .from('todo')
         .then((todos) => {
             res.render('all', { todos: todos });
         });
+});
+
+router.get('/new', (req, res) => {
+    res.render('new');
 });
 
 module.exports = router;
